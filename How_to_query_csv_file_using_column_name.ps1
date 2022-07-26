a7ecbd8b8395f55cbea65e19032c279acf6e36d6
rio197 <thomasvictorio@yahoo.com>
2022-07-26 10:38:49 -0400

    First commit
    First commit

---
+++ How_to_query_csv_file_using_column_name.ps1
@@ -0,0 +1,7 @@
+# Example: Search for the Deal name = 'SYZ Capital - Equity Hedge' from the CAR Report "Report.csv":
+
+# List the column names to query on
+PS C:\Users> Get-Content 'Report.csv' -Head 1
+
+# Query the .csv file using the column name
+PS C:\Users> 
