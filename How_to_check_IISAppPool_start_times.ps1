9348165e17af9a92239a29c9a520ef9e2ee2950a
rio197 <thomasvictorio@yahoo.com>
2022-09-29 10:09:44 -0400

    Add how to use in a remote call

--- How_to_check_IISAppPool_start_times.ps1
+++ How_to_check_IISAppPool_start_times.ps1
@@ -11,3 +11,6 @@ foreach ($AppPool in (Get-IISAppPool).WorkerProcesses.AppPoolName) {
 
 # As a one-liner:
 function MakeData($AppPool) { $out = new-object psobject; $out | add-member noteproperty AppPool $AppPool; $out | add-member noteproperty StartTime (ps -id (Get-IISAppPool -Name $AppPool).WorkerProcesses.ProcessId).StartTime; write-output $out }; foreach ($AppPool in (Get-IISAppPool).WorkerProcesses.AppPoolName) { MakeData $AppPool }
+
+# As a remote call:
+Invoke-Command -ComputerName HOSTNAME {function makeData($AppPool) ($out) = new-object psobject; $out | add-member noteproperty AppPool $AppPool; $out | add-member noteproperty StartTime (ps -id (Get-IISAppPool -Name $AppPool).WorkerProcesses.ProcessId).StartTime; write-output $out }; foreach ($AppPool in (Get-IISApPool).WorkerProcesses.AppPoolName) { MakeData $AppPool }} | Select PSComputerName,AppPool,StartTime | Sort-Object -Property StartTime -Descending
