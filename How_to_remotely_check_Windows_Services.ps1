f0b4d77fb0617ec5c273d004a0bc3484a88d8c4b
rio197 <thomasvictorio@yahoo.com>
2023-05-02 13:08:18 -0400

    Typo
    Typo

--- How_to_remotely_check_Windows_Services.ps1
+++ How_to_remotely_check_Windows_Services.ps1
@@ -1,5 +1,5 @@
-# How to remotely check Windows Servers
+# How to remotely check Windows Services
 PS C:\Users> Get-WmiObject win32_service -ComputerName HOSTNAME | where { $_.StartName -eq "AD\USERNAME" } | Select name, startname, startmode, state
 
-# How to locally check Windows Servers
+# How to locally check Windows Services
 PS C:\Users> Get-WmiObject win32_service | Select name, startname, startmode, state | select-string "w3|ism"
