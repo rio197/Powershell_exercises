04703ea18d7729726e5ff5cffa6acb1daa3b8b7f
rio197 <thomasvictorio@yahoo.com>
2023-05-02 13:08:00 -0400

    Add how to check locally
    Add how to check locally

--- How_to_remotely_check_Windows_Services.ps1
+++ How_to_remotely_check_Windows_Services.ps1
@@ -1,3 +1,5 @@
 # How to remotely check Windows Servers
-
 PS C:\Users> Get-WmiObject win32_service -ComputerName HOSTNAME | where { $_.StartName -eq "AD\USERNAME" } | Select name, startname, startmode, state
+
+# How to locally check Windows Servers
+PS C:\Users> Get-WmiObject win32_service | Select name, startname, startmode, state | select-string "w3|ism"
