5f406c91ef3269808d44bc65b571ae160040df83
rio197 <thomasvictorio@yahoo.com>
2023-05-02 13:24:41 -0400

    Add check process
    Add check process

--- How_to_remotely_check_Windows_Services.ps1
+++ How_to_remotely_check_Windows_Services.ps1
@@ -3,3 +3,6 @@ PS C:\Users> Get-WmiObject win32_service -ComputerName HOSTNAME | where { $_.Sta
 
 # How to locally check Windows Services
 PS C:\Users> Get-WmiObject win32_service | Select name, startname, startmode, state | select-string "w3|ism"
+
+# How to locally check IIS and ISAdmin processes
+PS C:\Users> Get-Process | Where-Object {($_.Name -like "*w3*") -or ($_.Name -like "*ism*")} | Select name,starttime
