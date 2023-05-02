4550d9a59e89454df9f09fb965d3caa011ddbc1a
rio197 <thomasvictorio@yahoo.com>
2023-05-02 13:39:34 -0400

    Add start times checks
    Add start times checks

--- How_to_remotely_check_Windows_Services.ps1
+++ How_to_remotely_check_Windows_Services.ps1
@@ -4,5 +4,15 @@ PS C:\Users> Get-WmiObject win32_service -ComputerName HOSTNAME | where { $_.Sta
 # How to locally check Windows Services
 PS C:\Users> Get-WmiObject win32_service | Select name, startname, startmode, state | select-string "w3|ism"
 
-# How to locally check IIS and ISAdmin processes
+# How to locally check Windows Services start times
+PS C:\Users> Get-WmiObject win32_service | where-object {($_.Name -like "*w3*") -or ($_.Name -like "*ism*")} -PipelineVariable Service | 
+ForEach-Object { 
+    Get-Process -Id $_.ProcessId | 
+    Select-Object -Property @{label='Status';expression={$Service.State}}, 
+                            @{label='Name';expression={$Service.Name}},
+                            @{label='DisplayName';expression={$Service.DisplayName}},
+                            StartTime
+}
+
+# How to locally check IIS processes
 PS C:\Users> Get-Process | Where-Object {($_.Name -like "*w3*") -or ($_.Name -like "*ism*")} | Select name,starttime
