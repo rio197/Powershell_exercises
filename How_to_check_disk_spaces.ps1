62ff2303d68c07e3dde41c8bfdc109124aa65d5b
rio197 <thomasvictorio@yahoo.com>
2023-05-24 14:27:32 -0400

    Update How_to_check_disk_spaces.ps1
    Add hostname check

--- How_to_check_disk_spaces.ps1
+++ How_to_check_disk_spaces.ps1
@@ -1,2 +1,2 @@
 # https://www.partitionwizard.com/resizepartition/powershell-get-disk-space.html
-Get-CimInstance -Class win32_logicaldisk | Format-Table DeviceId, MediaType, @{n="Size";e={[math]::Round($_.Size/1GB,2)}},@{n="FreeSpace";e={[math]::Round($_.FreeSpace/1GB,2)}}
+(hostname).toupper(); Get-CimInstance -Class win32_logicaldisk | Format-Table DeviceId, MediaType, @{n="Size";e={[math]::Round($_.Size/1GB,2)}},@{n="FreeSpace";e={[math]::Round($_.FreeSpace/1GB,2)}}
