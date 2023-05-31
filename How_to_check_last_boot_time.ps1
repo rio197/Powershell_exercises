5c7413a7dbce5ab1b8b4e8b33945d7c3935f9580
rio197 <thomasvictorio@yahoo.com>
2023-05-31 14:56:11 -0400

    Update How_to_check_last_boot_time.ps1
    Add new alternative

--- How_to_check_last_boot_time.ps1
+++ How_to_check_last_boot_time.ps1
@@ -1,2 +1,5 @@
 # https://geekflare.com/windows-last-boot-time/
 (hostname).toupper(); (gcim Win32_OperatingSystem).LastBootUpTime
+
+# https://devblogs.microsoft.com/scripting/powertip-get-the-last-boot-time-with-powershell/
+Get-CimInstance -ClassName win32_operatingsystem | select csname, lastbootuptime
