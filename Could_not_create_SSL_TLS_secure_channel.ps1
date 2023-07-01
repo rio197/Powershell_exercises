7b0282063d7aeb4fc4a947698835b442707aba8e
rio197 <thomasvictorio@yahoo.com>
2023-07-01 02:25:36 -0400

    Update Could_not_create_SSL_TLS_secure_channel.ps1
    Add VertiGIS reference

--- Could_not_create_SSL_TLS_secure_channel.ps1
+++ Could_not_create_SSL_TLS_secure_channel.ps1
@@ -1,4 +1,5 @@
 #https://learn.microsoft.com/en-us/windows/win32/secauthn/tls-cipher-suites-in-windows-8-1
+#https://support.vertigis.com/hc/en-us/articles/11461032941586-Windows-Server-2012-R2-and-Advanced-TLS-1-2-Cipher-Suites
 
 #https://stackoverflow.com/questions/41618766/powershell-invoke-webrequest-fails-with-ssl-tls-secure-channel
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
