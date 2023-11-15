f21e3599708191912abb038296aa8501b7bad94b
rio197 <thomasvictorio@yahoo.com>
2023-11-15 09:35:23 -0500

    First Commit
    First Commit

---
+++ How_to_check_Windows_Updates_history.ps1
@@ -0,0 +1,3 @@
+# How to check Windows Updates history
+# Reference: https://www.reddit.com/r/Windows10/comments/3iovtx/launch_windows_update_from_command_line/?rdt=63219
+C:\Windows\System32\control.exe /name Microsoft.WindowsUpdate
